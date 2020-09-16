c416cb57bf3ffbc83654be86af4a32ed2faf624f
Rifki Padilah <rpadilah90@gmail.com>
2020-09-16 11:06:55 +0700

    Jurnal1.java

---
+++ Praktikum 2/Jurnal1.java
@@ -0,0 +1,9 @@
+public class Jurnal1 {
+	
+	public static void main (String[] args) {
+		System.out.println ("Nama : Rifki Padilah");
+		System.out.println ("NIM  : 6706200054");
+		System.out.println ("Kelas: D3IF44-04");
+	}
+
+}
